19af002224ad01ae1b2c612450807b5328ebca5a
G-Just <gjustas00@gmail.com>
2024-04-19 13:10:35 +0300

    General update

--- src/pdfGenerator.php
+++ src/pdfGenerator.php
@@ -5,3 +5,21 @@ use Spatie\Browsershot\Browsershot;
 require __DIR__ . "/../vendor/autoload.php";
 
 Browsershot::url('http://localhost/PHP-pdf/src/pdf.html')->save('../docs/document.pdf');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDF</title>
+</head>
+
+<body style="height: 100vh; margin: 0; padding: 0; overflow: hidden;">
+    <iframe src="../docs/document.pdf" style="width: 100%;height: 100%;border: none;"></iframe>
+</body>
+
+</html>
